4e99bb905eea6c2f76ab526a26858dc37ac812cf
Erblin Gara <garaerblin@gmail.com>
2024-02-11 09:28:39 -0500

    learning repo

---
+++ Modules/Class/Variable.js
@@ -0,0 +1,23 @@
+let firstName = 'John';
+let lastName = 'Doe';
+
+let age = 27;
+let isDoorClosed = false;
+let fullName = 'John Doe';
+let time = null;
+
+
+
+ let person = {
+    firstName : 'John',
+    lastName : 'Doe',
+    age : 27
+ }
+
+ let cities = ['London', 'Paris', 'New York']
+
+ console.log(`My Name is ${fullName}`);
+console.log(`I am ${age} years old`);
+console.log(`I got the name ${firstName} from my grandpa`);
+ 
+ 
