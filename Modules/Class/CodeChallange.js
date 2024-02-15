b57fe530d903ad4b828acb6f89f4b0814b593883
Erblin Gara <garaerblin@gmail.com>
2024-02-15 00:36:26 -0500

    Code challange

---
+++ Modules/Class/CodeChallange.js
@@ -0,0 +1,29 @@
+let firstName = 'Jim';
+let lastName = 'Carry';
+let fullName = 'Jim Carry';
+
+console.log(typeof fullName );
+console.log(`My name is ${fullName}`);
+
+let age = 25;
+
+console.log(typeof age);
+console.log(`I am ${age} years old`);
+
+let isStudent = true
+
+console.log(typeof isStudent);
+console.log(isStudent);
+
+let favoriteColors = ['red', 'green', 'blue'];
+
+console.log(typeof favoriteColors);
+console.log(`my favorite colors are ${favoriteColors}`)
+
+let person = {
+    firstName : 'Jim',
+    lastName : 'Carry',
+    age : 25,
+    isStudent : true
+};
+console.log(person);
