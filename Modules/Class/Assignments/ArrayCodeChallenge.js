9374f2485823bb82546bbfe59b036a06b7997031
Erblin Gara <garaerblin@gmail.com>
2024-02-26 02:09:35 -0500

    Arrays

---
+++ Modules/Class/Assignments/ArrayCodeChallenge.js
@@ -0,0 +1,30 @@
+let studentNames = ['Kate', 'Jim', 'Chriss', 'George', 'Jasmine'];
+
+console.log(studentNames.length);
+
+//let poppedValue = studentNames.pop();
+//let poppedValue = studentNames.shift();
+//let poppedValue = studentNames.unshift('Monica');
+//let poppedValue = studentNames.push('Julie');
+//let newNames = studentNames.splice(4, 1, 'Edward')
+
+//let isIncludesJohn = studentNames.includes('John');
+//console.log(isIncludesJohn);
+
+//let indexOfKate = studentNames.indexOf('Kate');
+//console.log(indexOfKate);
+
+//I tried to do 2 ways on this one.
+//studentNames.reverse();
+//let lastIndexOf = studentNames.lastIndexOf('Kate');
+//console.log(lastIndexOf);
+//or,
+//console.log(studentNames.lastIndexOf('Kate'));
+
+//console.log(studentNames.indexOf('Kate') == studentNames.indexOf('Jasimne'));
+
+
+
+
+
+                                              
