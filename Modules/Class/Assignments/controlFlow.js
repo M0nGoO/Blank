f60ee2f0812f00cb0fc902dc6f2aa431fbf78cc6
Erblin Gara <garaerblin@gmail.com>
2024-02-28 10:38:52 -0500

    Control flow assignment

---
+++ Modules/Class/Assignments/controlFlow.js
@@ -0,0 +1,64 @@
+// let number = 10;
+
+// if(number % 2 === 0){
+//     console.log(`'number ${number} is even'`)
+// } else {
+//     console.log(`'number ${number} is odd'`)
+// }
+
+
+
+// let age = 24;
+
+// if(age < 18){
+//     console.log('You are a minor')
+// } else if (age >= 18 && age <= 65 ){
+//     console.log('You are an adult')
+// } else if (age > 65) {
+//     console.log('You are a senior citizen')
+// }
+
+
+
+// let letter = 'e'
+
+// if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+//     console.log(`'letter "${letter}" is a vowel'`)
+// } else {
+//     console.log(`'letter "${letter}" is a consonant'`)
+// }
+
+
+
+let numberOne = 30;
+let numberTwo = 15;
+let numberThree = 45;
+
+if (numberTwo >= numberOne) {
+    console.log('15 is the largest number')
+} else if (numberThree >= numberOne){
+    console.log('45 is the highest number')
+}
+
+
+
+
+
+//  let password = 'Test1233';
+//  const regex = new RegExp('\^[A-Za-z0-9\]+$');
+
+
+// if(password.length <= 8) {
+//     console.log('Pasword is too short')
+//  }
+// if (regex.test(password)){
+//      console.log('password accepted')
+//  } else if (regex.test(!password)){
+//  console.log('password rejected')
+//  }
+
+// if(password == 'Test1233') {
+//     console.log('Password is correct')
+// } else if (password !== 'Test1233') {
+//     console.log('Password is incorrect, please try again')
+// }
