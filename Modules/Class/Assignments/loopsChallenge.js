7fc0ad6fbda68caf2a9c498d91555ba05301e274
Erblin Gara <garaerblin@gmail.com>
2024-03-10 13:06:46 -0400

    Loops

---
+++ Modules/Class/Assignments/loopsChallenge.js
@@ -0,0 +1,73 @@
+let products = [
+    { name: "Apples", price: 10, quantity: 5 },
+    { name: "Bananas", price: 20, quantity: 3 },
+    { name: "Oranges", price: 15, quantity: 7 },
+    { name: "Kiwis", price: 25, quantity: 2 },
+    { name: "Watermelons", price: 30, quantity: 4 }
+]
+
+
+
+
+// let total = 0;
+
+// for(let productInfo of products) {
+//     total += productInfo.price
+// } 
+
+// console.log (`The total of all products is $${total}`);
+
+// for(let value of products) {
+//     console.log(value.name,":" + "$" + value.price);
+//}
+
+
+
+// let cheapestProduct = products[0];
+// let productsLength = products.length;
+// let i = 1;
+
+// while(i < productsLength) {
+//     if (products[i].price < cheapestProduct.price) {
+//         cheapestProduct = products[i];
+//     }
+//     i++;
+// }
+// console.log(` ${cheapestProduct.name} is the cheapest product at $${cheapestProduct.price}`);
+
+
+
+
+// for(let i = 0; i < products.length; i++) {
+//     sum += products[i].price 
+// }  console.log (`Total value of all items: $${sum}`)
+
+
+
+
+// console.log('Products Detail :');
+// for (let productInfo of products) {
+//     // console.log(productInfo); { name: "Apples", price: 10, quantity: 5 }
+//     for (let productAttributes in productInfo) {
+//         let temp = productAttributes.charAt(0).toUpperCase() + productAttributes.substring(1).toLowerCase()
+//         console.log(`${temp} : ${productInfo[productAttributes]}`);
+//     }
+//     console.log("\n");
+// }
+
+
+// for(let i = 1; i < 101; i++) {
+//     if ( i % 15 == 0)
+//         console.log("FizzBuzz");
+//     else if ( i % 3 == 0)
+//         console.log("Fizz");
+//     else if ( i % 5 == 0)
+//         console.log("Buzz");
+//     else
+//         console.log(i);     
+// }
+
+
+
+
+
