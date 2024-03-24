b057f771611f37771c9b99f9ad4c26c20279ba96
Erblin Gara <garaerblin@gmail.com>
2024-03-24 11:55:25 -0400

    spread challenge

---
+++ Modules/Class/Assignments/spreadChallenge.js
@@ -0,0 +1,42 @@
+
+//***************************
+// let arrayOne = [1, 2, 3];
+// let arrayTwo = [4, 5, 6];
+
+// let newArray = [...arrayOne, ...arrayTwo];
+// console.log(newArray);
+
+//******************************* 
+// let originalArray = [ 'banana', 'apple', 'cherry'];
+
+// let newOriginalArray = ['kiwi', ...originalArray]
+// console.log(newOriginalArray);
+
+
+//***************************** 
+
+// let originalObject = {name: 'John', age: 30};
+// Object.assign (originalObject, {gender: 'Male'});
+
+// console.log(originalObject);
+
+
+//*******************************
+
+// let numbersArray = [4, 8, 13, 18, 54, 9];
+// let arrLength = numbersArray.length;
+// let largest = numbersArray[0];
+
+// for(let i = 0; i < arrLength; i++ ) {
+//     if (numbersArray[i] > largest) {
+//         largest = numbersArray[i];
+//     }
+// }
+// console.log(largest);
+
+// *****************************
+
+let originalArray = ['banana', 'apple', 'cherry'];
+
+let sortedArray = originalArray.sort();
+console.log(sortedArray);
